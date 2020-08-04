b735f0da880490998999a8f8f02c99262848bae4
905616089 <57530010+905616089@users.noreply.github.com>
2020-08-04 20:21:28 +0800

    完善

--- boke.js
+++ boke.js
@@ -3,6 +3,7 @@ var one=document.querySelector(".one");
 var two=document.querySelector(".two");
 var closes=document.querySelector(".close");
 
+
 topButton.onmouseover=function(){
     
     one.style.transform="rotate(30deg)";
@@ -24,3 +25,17 @@ topButton.onclick=function(){
 closes.onclick=function(){
     $(".navigationWeb").animate({right:-400+"px"},500)
 }
+//博客侧边栏
+var title=document.querySelectorAll(".center-left1");
+title[0].style.display="block";
+$(".title2-text").click(function(){
+    var x=$(".title2-text").index(this);
+    var lenght=$(".title2-text").length;
+    console.log(lenght)
+
+      $(".center-left1").css({
+            display:"none"
+        })  
+    title[x].style.display="block";
+    
+})
--- center.js
+++ center.js
@@ -25,3 +25,46 @@ topButton.onclick=function(){
 closes.onclick=function(){
     $(".navigationWeb").animate({right:-400+"px"},500)
 }
+
+//按钮
+var btn1=document.querySelector(".btn1");
+var btn2=document.querySelector(".btn2");
+var img=document.querySelectorAll(".imglist-div img");
+
+
+
+btn2.onclick=function(){
+    for(var i=0;i<img.length;i++){
+    
+        var link=img[i].src;
+        img[i].src=img[i].getAttribute("other");
+        console.log(link);
+        img[i].other=link;
+        console.log(img[i].other);
+    }
+    btn2.style.background="black";
+    btn2.style.color="white";
+    btn1.style.background="white"
+    btn1.style.color="black";
+}
+btn1.onclick=function(){
+    for(var i=0;i<img.length;i++){
+    
+        var link=img[i].src;
+        img[i].src=img[i].other
+        img[i].other=link;
+    }
+    btn1.style.background="black";
+    btn1.style.color="white";
+    btn2.style.background="white"
+    btn2.style.color="black";
+}
+
+
+
+
+
+
+
+
+
